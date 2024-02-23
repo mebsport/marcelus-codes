c676d842780ed95cb52dd37cbe8afd811aefc05e
mebsport <56307645+mebsport@users.noreply.github.com>
2024-02-23 10:28:33 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col bg-black'>
       <div className='container mx-auto py-4 px-12'>
-      <HeroSection/>
+        <HeroSection />
       </div>
     </main>
   )
