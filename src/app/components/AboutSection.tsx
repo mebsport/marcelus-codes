ece67a4d534dcbfb9176277f37104e62126b8762
mebsport <56307645+mebsport@users.noreply.github.com>
2024-03-01 11:21:46 -0500

    Update AboutSection.tsx

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -51,22 +51,22 @@ const AboutSection = () => {
   ]
 
   return (
-    <section className='text-white'>
+    <section className='text-white' id='about'>
       <div className='md:grid md:grid-cols-2 gap-8 py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
         <div>
-        <Image
-          src='/images/Marcelus_Coding_Robotics.jpg'
-          alt='Marcelus Coding At Robotics Competition'
-          width={600}
-          height={600}
-        />
-        <Image
-        className='mt-4'
-        src='/images/Fun_Robotics_Coding.jpg'
-        alt='Fun Coding At Robotics Competition'
-        width={600}
-        height={600}
-        />
+          <Image
+            src='/images/Marcelus_Coding_Robotics.jpg'
+            alt='Marcelus Coding At Robotics Competition'
+            width={600}
+            height={600}
+          />
+          <Image
+            className='mt-4'
+            src='/images/Fun_Robotics_Coding.jpg'
+            alt='Fun Coding At Robotics Competition'
+            width={600}
+            height={600}
+          />
         </div>
         <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
           <h2 className='text-4xl fonot-bold text-white mb-4'>About Me</h2>
