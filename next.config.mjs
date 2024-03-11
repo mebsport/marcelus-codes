d8a712e40eb7376c1facae81cbeeb234e85efd8e
mebsport <56307645+mebsport@users.noreply.github.com>
2024-03-11 09:28:25 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.infrastructureLogging = { debug: /PackFileCache/ }
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
